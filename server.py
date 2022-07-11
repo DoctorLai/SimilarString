167a4b49e5b3d2df2281c7871136fa16341faddf
DoctorLai <dr.zhihua.lai@gmail.com>
2022-07-11 20:11:30 +0100

    Create server.py

---
+++ server.py
@@ -0,0 +1,44 @@
+import json
+from sentence_transformers import SentenceTransformer, util
+import numpy as np
+from flask import Flask, request
+
+app = Flask(__name__)
+
+print("Loading ML model...")
+model = SentenceTransformer('stsb-roberta-large')
+print("OK!")
+
+sentence1 = "I like Python because I can build AI applications"
+sentence2 = "I like Python because I can do data analytics"
+# encode sentences to get their embeddings
+embedding1 = model.encode(sentence1, convert_to_tensor=True)
+embedding2 = model.encode(sentence2, convert_to_tensor=True)
+# compute similarity scores of two embeddings
+cosine_scores = util.pytorch_cos_sim(embedding1, embedding2)
+print("Sentence 1:", sentence1)
+print("Sentence 2:", sentence2)
+print("Similarity score:", cosine_scores.item())
+
+@app.route('/', methods=['POST']) 
+def score():
+    data = request.get_json(force=True)
+    print(data)
+    sentence1 = data['s1']
+    sentence2 = data['s2']
+    if 'test' in data:
+        score = data['test']
+    else:    
+        embedding1 = model.encode(sentence1, convert_to_tensor=True)
+        embedding2 = model.encode(sentence2, convert_to_tensor=True) 
+        cosine_scores = util.pytorch_cos_sim(embedding1, embedding2)   
+        score = cosine_scores.item()
+    data = json.dumps({
+        "score": score
+    })
+    print(data)
+    return data 
+
+if __name__ == "__main__":
+    print("Starting the server....")    
+    app.run()
