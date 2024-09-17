f56ab277b2ca54b7556fe8e0840d0019855304fa
justyy <dr.zhihua.lai@gmail.com>
2024-09-17 11:06:26 +0000

    Refactor

--- test_ml_server.sh
+++ test_ml_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+set -ex
 
-set -x
-curl -X POST -H "Content-type:application/json"  --data '{"s1":"This is a Surface Studio Laptop","s2":"That is a car"}' http://127.0.0.1:5000
+curl -v -H "Content-type:application/json"  --data '{"s1":"This is a Surface Studio Laptop","s2":"That is a car"}' http://127.0.0.1:5000
 
